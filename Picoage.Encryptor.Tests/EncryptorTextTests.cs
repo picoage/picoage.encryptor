aff46c9025b5f71a49b74b31beb01009cca9d1d6
Vishnu Kumar <microsophtdeveloper@outlook.com>
2018-12-10 21:19:34 +0000

    unit test added

---
+++ Picoage.Encryptor.Tests/EncryptorTextTests.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Picoage.Encryptor.Tests
+{
+    [TestClass]
+    public class EncryptorTextTests
+    {
+        [TestMethod]
+        public void When_PlanTextIsGiven_Then_EncyptText()
+        {
+            //Arrange 
+            byte[] key = { 145, 12, 32, 245, 98, 132, 98, 214, 6, 77, 131, 44, 221, 3, 9, 50 };
+            byte[] iv = { 15, 122, 132, 5, 93, 198, 44, 31, 9, 39, 241, 49, 250, 188, 80, 7 };
+            string name = "Password";
+
+            //Act
+            string chiperText = EncryptorText.EncryptPlainText(key, iv, name);
+            string plainText = EncryptorText.DecryptChiperText(key, iv, chiperText);
+
+            //Assert
+            Assert.AreEqual(name, plainText); 
+        }
+    }
+}
---
+++ src/Picoage.Encryptor/EncryptorText.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Picoage.Encryptor
+{
+    public static class EncryptorText
+    {
+        public static string EncryptPlainText(byte[] key, byte[] iv, string value)
+        {
+            byte[] plainTextInBytes = Encoding.UTF8.GetBytes(value);
+
+            using (Aes algorithm = Aes.Create())
+            {
+                using (ICryptoTransform encryptor = algorithm.CreateEncryptor(key, iv))
+                {
+                    return Convert.ToBase64String(Crypt(plainTextInBytes, encryptor));
+                }
+            }
+        }
+
+        public static string DecryptChiperText(byte[] key, byte[] iv, string chiperText)
+        {
+            byte[] chiperTextInBytes = Convert.FromBase64String(chiperText);
+            using (Aes algorithm = Aes.Create())
+            {
+                using (ICryptoTransform decryptor = algorithm.CreateDecryptor(key, iv))
+                {
+                    return Encoding.UTF8.GetString(Crypt(chiperTextInBytes, decryptor));
+                }
+            }
+        }
+
+        private static byte[] Crypt(byte[] data, ICryptoTransform cryptor)
+        {
+            MemoryStream memoryStream = new MemoryStream();
+
+            using (Stream stream = new CryptoStream(memoryStream, cryptor, CryptoStreamMode.Write))
+            {
+                stream.Write(data, 0, data.Length);
+            }
+            return memoryStream.ToArray();
+        }
+    }
+}
